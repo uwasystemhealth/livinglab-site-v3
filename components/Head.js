4e62bc09a40f78d15075129d38ac9f17e8576587
frinzekt <frinzekt@gmail.com>
2020-06-18 10:45:53 +0800

    initial component split

---
+++ components/Head.js
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+const HeadMeta = () => {
+	return (
+		<Head>
+			<title>Create Next App</title>
+			<link rel='icon' href='/favicon.ico' />
+			<link rel='stylesheet' href='css/style.css' />
+		</Head>
+	);
+};
+
+export default HeadMeta;
