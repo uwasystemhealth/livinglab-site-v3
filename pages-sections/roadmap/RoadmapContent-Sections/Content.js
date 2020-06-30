3d8c3bf6c19dc30cc26c7a597824fbc7b03eea0d
frinzekt <frinzekt@gmail.com>
2020-06-30 12:52:15 +0800

    roadmap pathway page
    contains the navigation and the actual content of the site

---
+++ pages-sections/roadmap/RoadmapContent-Sections/Content.js
@@ -0,0 +1,44 @@
+import React, { Fragment } from 'react';
+// @material-ui/core components
+import { makeStyles } from '@material-ui/core/styles';
+
+// @material-ui/icons
+import Icon from '@material-ui/core/Icon';
+
+// core components
+import Card from 'components/MaterialKit/Card/Card.js';
+import CardBody from 'components/MaterialKit/Card/CardBody.js';
+import CardHeader from 'components/MaterialKit/Card/CardHeader.js';
+import GridContainer from 'components/MaterialKit/Grid/GridContainer.js';
+import GridItem from 'components/MaterialKit/Grid/GridItem.js';
+
+import styles from 'assets/jss/nextjs-material-kit/pages/landingPageSections/productStyle.js';
+
+const useStyles = makeStyles(styles);
+
+const ALTDescription = ({ requirements }) => {
+	const classes = useStyles();
+	return (
+		<Fragment>
+			<GridContainer className={classes.section}>
+				{requirements.map(({ title, content }, index) => (
+					<GridItem key={title} md={index == requirements.length - 1 && requirements.length % 2 ? 12 : 6}>
+						{/* RULE FOR LAST ELEMENT TO EXPAND IF ODD NUMBER OF ITEMS */}
+						<Card>
+							<CardHeader color='success'>
+							{/* <Icon>add_circle</Icon> */}
+								<h4>{title}</h4>
+							</CardHeader>
+							<CardBody>
+							
+								<h5 className={classes.description} dangerouslySetInnerHTML={{ __html: content.replace(/\n/g, '<br/><br/>') }}></h5>
+							</CardBody>
+						</Card>
+					</GridItem>
+				))}
+			</GridContainer>
+		</Fragment>
+	);
+};
+
+export default ALTDescription;
