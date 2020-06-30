6dbe5d713f127f6de68f4fa631dea4fb7549ba16
frinzekt <frinzekt@gmail.com>
2020-06-30 13:30:13 +0800

    copy code from the derisking site trials here
    the copied code is for the reason of flexiblity. Copied code can be modified on specific pages without changing the other pages. There is a share component for derisking site trials and accelerated life testing with "RoadmapPathwayPage" component

--- pages/roadmap/accelerated-life-testing/index.js
+++ pages/roadmap/accelerated-life-testing/index.js
@@ -5,7 +5,17 @@ import React, { Component, Fragment } from 'react';
 import classNames from 'classnames';
 
 // @material-ui/core components
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
+import Stepper from '@material-ui/core/Stepper';
+import Step from '@material-ui/core/Step';
+import StepLabel from '@material-ui/core/StepLabel';
+import StepContent from '@material-ui/core/StepContent';
+import StepConnector from '@material-ui/core/StepConnector';
+
+//ICONS
+import PartnerIcon from '@material-ui/icons/SupervisorAccount';
+import SafetyIcon from '@material-ui/icons/LocalHospital';
+import DraftsIcon from '@material-ui/icons/Drafts';
 
 // CORE COMPONENTS
 import GridContainer from 'components/MaterialKit/Grid/GridContainer.js';
@@ -17,11 +27,23 @@ import Parallax from 'components/MaterialKit/Parallax/Parallax.js';
 import ALTDescription from 'pages-sections/accelerated-life-testing/AcceleratedLifeTesting-Sections/Description.js';
 import Testings from 'pages-sections/accelerated-life-testing/AcceleratedLifeTesting-Sections/Testings.js';
 
+// DATA Import
+import DERISKING_SITE_TRIALS_DATA from 'data/Roadmap-DeriskingSiteTrials.json';
+
 import styles from 'assets/jss/nextjs-material-kit/pages/landingPage.js';
+import stepperStyles from 'assets/jss/custom/stepper.js';
+
 const useStyles = makeStyles(styles);
+const useStepperStyles = makeStyles(stepperStyles);
+
 
 const AcceleratedLifeTesting = () => {
 	const classes = useStyles();
+	const stepperClasses = useStepperStyles();
+	const stepIcon = (props) => {
+		const icons = [<PartnerIcon></PartnerIcon>, <PartnerIcon></PartnerIcon>, <SafetyIcon></SafetyIcon>, <DraftsIcon></DraftsIcon>];
+		return <div className={stepperClasses.root}>{icons[props.icon - 1]}</div>;
+	};
 	return (
 		<Fragment>
 			<Parallax small filter responsive image={require('assets/img/landing-bg.jpg')}>
@@ -36,7 +58,28 @@ const AcceleratedLifeTesting = () => {
 			<div className={classNames(classes.main, classes.mainRaised)}>
 				<div className={classes.container}>
 					<ALTDescription></ALTDescription>
-					<Testings></Testings>
+					<Stepper orientation='vertical'>
+						{DERISKING_SITE_TRIALS_DATA.map(({ title, description }) => (
+							<Step key={title} active={true}>
+								<StepLabel StepIconComponent={stepIcon}>{title}</StepLabel>
+								<StepContent>
+									<div>{description}</div> <br />
+									<GridContainer justify='flex-end'>
+										<GridItem xs={9} md={3}>
+											<Button
+												color='primary'
+												href={`accelerated-life-testing/${title.toLowerCase().replace(/ /g, '-')}`}
+												style={{ width: '100%' }}
+											>
+												More About <br />
+												{title}
+											</Button>
+										</GridItem>
+									</GridContainer>
+								</StepContent>
+							</Step>
+						))}
+					</Stepper>
 				</div>
 			</div>
 		</Fragment>
