239af89f27a3965f5f7c1d2b75b00a78271613e5
frinzekt <frinzekt@gmail.com>
2020-07-01 13:30:57 +0800

    change fonts

--- assets/jss/nextjs-material-kit.js
+++ assets/jss/nextjs-material-kit.js
@@ -210,11 +210,11 @@ const anchor = {
 	visibility: "hidden"
 }
 const title = {
-  color: "#3C4858",
-  margin: "1.75rem 0 0.875rem",
-  textDecoration: "none",
-  fontWeight: "700",
-  fontFamily: `"Roboto Slab", "Times New Roman", serif`
+	color: '#3C4858',
+	margin: '1.75rem 0 0.875rem',
+	textDecoration: 'none',
+	fontWeight: '700',
+	fontFamily: `"Arial", "Times New Roman", serif`,
 };
 
 const cardTitle = {
