fda4247f2d7236931a8ac49b3ce609b9546c4cdf
frinzekt <frinzekt@gmail.com>
2020-07-02 12:09:33 +0800

    about us page content section
    This needs more information in terms of the teams of the living lab

--- pages-sections/accelerated-life-testing/AcceleratedLifeTesting-Sections/Description.js
+++ pages-sections/accelerated-life-testing/AcceleratedLifeTesting-Sections/Description.js
@@ -17,8 +17,18 @@ const ALTDescription = () => {
 		<div className={classes.section}>
 			<h2 className={classes.title}>Testing In The Living Lab</h2>
 			<h5 className={classes.description}>
-				Some Text HereSome Text HereSome Text HereSome Text HereSome Text HereSome Text HereSome Text HereSome Text HereSome Text HereSome
-				Text HereSome Text HereSome Text HereSome Text HereSome Text Here Some Text Here Some Text HereSome Text HereSome Text Here
+				Accelerated aging and testing is used to simulate product failure in a short period of time, ideally before the products are in the
+				marketplace. Traditionally, it uses unusually high levels of environmental conditions to speed up the aging processes using a
+				standard test methodology.
+			</h5>
+			<h5 className={classes.description}>
+				The Living Lab has access to a wide range of expensive test equipment on the UWA Campus as well as the ability to design/ repurpose
+				equipment for specific accelerated life trials.
+			</h5>
+			<h5 className={classes.description}>
+				We are not an accredited test laboratory and do not seek to compete with these highly specialised, ISO 9001 compliant businesses.
+				Our aim is to help the product developer understand the vulnerabilities in their product during the design and prototyping phases so
+				their product is compliant when they seek accreditation against Standards.
 			</h5>
 			<Button color='success' href='/roadmap/accelerated-life-testing'>
 				More About Pathway
