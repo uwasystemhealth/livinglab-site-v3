dfa5e76e575c3f9a64b27fd3baa459095ae2dc4b
frinzekt <frinzekt@gmail.com>
2020-07-06 17:22:53 +0800

    privacy policy content

--- pages-sections/PrivacyPolicy-Sections/PrivacyPolicyContent.js
+++ pages-sections/PrivacyPolicy-Sections/PrivacyPolicyContent.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 // @material-ui/core components
 import { makeStyles } from '@material-ui/core/styles';
 
@@ -17,9 +17,35 @@ const useStyles = makeStyles(styles);
 const About = () => {
 	const classes = useStyles();
 	return (
-		<div className={classes.section}>
-			<h5 className={classes.description}>[Insert Privacy Policy Here]</h5>
-		</div>
+		<Fragment>
+			<div className={classes.section}>
+				<h2 className={classes.title}>Information we collect</h2>
+				<h5 className={classes.description}>
+					When you visit our website, we do not collect information regarding your browsing information. However, if you fill in and
+					submitted the contact form, we will be collecting information limited to:
+					<ul>
+						<li>Your Business Information such as: Business Name, Business Website, Business Mailing Address</li>
+						<li>Your Contact Information such as: Contact Name, Contact Phone, Contact Email</li>
+					</ul>
+				</h5>
+			</div>
+			<div className={classes.section}>
+				<h2 className={classes.title}>How We Store Information</h2>
+				<h5 className={classes.description}>
+					We do not keep information that we no longer need for the purpose it was collected, unless the law requires us to do so. It is
+					either destroyed or de-identified as required under the Archives Act 1983. Only authorised Health staff can access personal
+					information stored by us.
+				</h5>
+				<h5 className={classes.description}>
+					Security features protect it from unauthorised access. We store information in different ways, including:
+					<ul>
+						<li>our document and records management systems</li>
+						<li>cloud storage</li>
+					</ul>
+					We do not store nor use cookies.
+				</h5>
+			</div>
+		</Fragment>
 	);
 };
 
