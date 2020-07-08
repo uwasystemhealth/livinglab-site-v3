0a29a20b8374c978b9aec1bced08536bde0f241f
frinzekt <frinzekt@gmail.com>
2020-07-08 17:42:18 +0800

    server side and client side form validation

--- components/Context/index.js
+++ components/Context/index.js
@@ -14,36 +14,97 @@ export const AppProvider = (props) => {
 		'Contact Name': '',
 		'Contact Phone': '',
 	});
+	const [contactFormErrorState, setContactFormErrorState] = useState({
+		'Business Mailing Address': '',
+		'Business Name': '',
+		'Business Website': '',
+		'Contact Email': '',
+		'Contact Name': '',
+		'Contact Phone': '',
+	});
 
 	const handleContactFormChange = (event) => {
 		const { name, value } = event.target;
 		const newState = { ...contactFormState, [name]: value };
 		setContactFormState(newState);
+
+		const validateModifyState = (value, validator, error) => {
+			if (!validator(value) && (!!value || validator == notEmpty)) {
+				setContactFormErrorState({ ...contactFormErrorState, [name]: error });
+			} else {
+				setContactFormErrorState({ ...contactFormErrorState, [name]: '' });
+			}
+		};
+
+		switch (name) {
+			case 'Contact Email':
+				validateModifyState(value, notEmpty, 'Required Field');
+				validateModifyState(value, validateEmail, 'Invalid Email');
+				break;
+			case 'Contact Phone':
+				validateModifyState(value, notEmpty, 'Required Field');
+				validateModifyState(value, validatePhone, 'Invalid Phone Number');
+				break;
+			case 'Business Website':
+				validateModifyState(value, validateWebsite, 'Invalid Website');
+				break;
+			default:
+				validateModifyState(value, notEmpty, 'Required Field');
+		}
+
 		console.log(newState);
 	};
 
-	const handleFormSubmit = async (e, roadmap = '') => {
+	const closeNotification = () =>
+		setNotificationState({
+			open: false,
+			text: '',
+			severity: 'success',
+		});
+
+	const handleFormSubmit = async (e) => {
 		e.preventDefault();
 		console.log(contactFormState);
 		try {
+			setNotificationState({
+				open: true,
+				text: 'Submitting... Wait For A Second',
+				severity: 'info',
+			});
+
 			await axios({
 				method: 'POST',
 				url: '/api/mail',
 				data: contactFormState,
 			});
 
-			if (roadmap) {
-				// GETTING A DOWLOADABLE
-				await axios({
-					method: 'POST',
-					url: '/api/mail',
-					data: { ...contactFormState, downloadables: roadmap },
-				});
-			}
+			// RESET STATE
+			setContactFormState({
+				'Business Mailing Address': '',
+				'Business Name': '',
+				'Business Website': '',
+				'Contact Email': '',
+				'Contact Name': '',
+				'Contact Phone': '',
+			});
 
-			console.log('Success');
+			//C	LOSE IT FIRST- THEN OPEN
+			setNotificationState({
+				open: false,
+			});
+
+			setNotificationState({
+				open: true,
+				text: 'Successfully sent',
+				severity: 'success',
+			});
+			setIsContactFormModalOpen(false);
 		} catch (err) {
-			console.log(err);
+			setNotificationState({
+				open: true,
+				text: `Error Encountered: ${err}`,
+				severity: 'error',
+			});
 		}
 	};
 
--- helpers/validation.js
+++ helpers/validation.js
@@ -3,6 +3,15 @@ export const validateEmail = (email) => {
 	return re.test(String(email).toLowerCase());
 };
 
+export const validateWebsite = (website) => {
+	const re = /[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/ig;
+	return re.test(String(website));
+};
+export const validatePhone = (phone) => {
+	const re = /^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$/g;
+	return re.test(String(phone));
+};
+
 export const notEmpty = (value) => !!value;
 
 export const stringToLink = (string) => string.toLowerCase().replace(/ /g, '-');
