9a97cd608979e058b509ea631e8fb2ea31603aad
frinzekt <frinzekt@gmail.com>
2020-07-09 17:56:58 +0800

    case studies modal card combo

---
+++ components/case-studies/CSModal.js
@@ -0,0 +1,69 @@
+import React, { useContext } from 'react';
+// material-ui components
+import withStyles from '@material-ui/core/styles/withStyles';
+import Slide from '@material-ui/core/Slide';
+import Dialog from '@material-ui/core/Dialog';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogActions from '@material-ui/core/DialogActions';
+import IconButton from '@material-ui/core/IconButton';
+
+// FORM Material UI
+import TextField from '@material-ui/core/TextField';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import Divider from '@material-ui/core/Divider';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { useTheme } from '@material-ui/core/styles';
+
+// @material-ui/icons
+import { makeStyles } from '@material-ui/core';
+import Close from '@material-ui/icons/Close';
+import People from '@material-ui/icons/People';
+
+// core components
+import Button from 'components/MaterialKit/CustomButtons/Button.js';
+import CustomInput from 'components/MaterialKit/CustomInput/CustomInput.js';
+import GridContainer from 'components/MaterialKit/Grid/GridContainer.js';
+import GridItem from 'components/MaterialKit/Grid/GridItem.js';
+
+// OWN COMPONENTS
+import CSPageContent from 'components/case-studies/CSPageContent.js';
+
+import modalStyle from 'assets/jss/nextjs-material-kit/modalStyle.js';
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+	return <Slide direction='down' ref={ref} {...props} />;
+});
+
+const useStyles = makeStyles(modalStyle);
+
+const TestingModal = (props) => {
+	const { isModalOpen, closeModal, ...rest } = props;
+	const theme = useTheme();
+	const fullScreenMobile = useMediaQuery(theme.breakpoints.down('sm'));
+	const classes = useStyles();
+
+	return (
+		<Dialog
+			classes={{
+				root: classes.center,
+				paper: classes.modal,
+			}}
+			open={isModalOpen}
+			TransitionComponent={Transition}
+			keepMounted
+			disableBackdropClick
+			fullWidth
+			fullScreen={fullScreenMobile}
+			onClose={closeModal}
+			aria-labelledby='modal-slide-title'
+			aria-describedby='modal-slide-description'
+			maxWidth='lg'
+			scroll='body'
+		>
+			<CSPageContent {...rest} closeModal={closeModal}></CSPageContent>
+		</Dialog>
+	);
+};
+
+export default TestingModal;
---
+++ components/case-studies/CSPageContent.js
@@ -0,0 +1,62 @@
+// NEXT + REACT
+import React, { Component, Fragment } from 'react';
+
+// nodejs library that concatenates classes
+import classNames from 'classnames';
+
+// @material-ui/core components
+import { makeStyles } from '@material-ui/core/styles';
+import IconButton from '@material-ui/core/IconButton';
+
+// @material-ui/icons
+import Close from '@material-ui/icons/Close';
+
+// CORE COMPONENTS
+import GridContainer from 'components/MaterialKit/Grid/GridContainer.js';
+import GridItem from 'components/MaterialKit/Grid/GridItem.js';
+import Button from 'components/MaterialKit/CustomButtons/Button.js';
+import Parallax from 'components/MaterialKit/Parallax/Parallax.js';
+
+import landingStyles from 'assets/jss/nextjs-material-kit/pages/landingPage.js';
+import productStyles from 'assets/jss/nextjs-material-kit/pages/landingPageSections/productStyle.js';
+import modalStyle from 'assets/jss/nextjs-material-kit/modalStyle.js';
+
+const useStylesLanding = makeStyles(landingStyles);
+const useStylesProduct = makeStyles(productStyles);
+const useStylesModal = makeStyles(modalStyle);
+
+const About = (props) => {
+	const { title, backgroundImage = 'img/feature-bg.jpg', description, videoLink, closeModal } = props;
+	const landingClasses = useStylesLanding();
+	const productClasses = useStylesProduct();
+	const modalClasses = useStylesModal();
+	return (
+		<Fragment>
+			<Parallax small responsive filter image={backgroundImage} style={closeModal ? { transform: 'translate3d(0,0px,0)' } : null}>
+				<div className={landingClasses.container}>
+					{closeModal && (
+						<IconButton className={modalClasses.modalCloseButton} key='close' aria-label='Close' color='inherit' onClick={closeModal}>
+							<Close className={modalClasses.modalClose} />
+						</IconButton>
+					)}
+					<h1 className={landingClasses.title}>{title}</h1>
+				</div>
+			</Parallax>
+			<div className={classNames(landingClasses.main, landingClasses.mainRaised)}>
+				<div className={productClasses.section}>
+					{/* <h2 className={classes.title}>Work In Progress</h2> */}
+					<GridContainer>
+						<GridItem xs={12} md={6} className={productClasses.gridItemPadded}>
+							<h5 className={classNames(productClasses.description, productClasses.descriptionContainer)}>{description}</h5>
+						</GridItem>
+						<GridItem xs={12} md={6} className={productClasses.gridItemPadded}>
+							<iframe className={productClasses.video} src={videoLink} allowFullScreen='allowFullScreen'></iframe>
+						</GridItem>
+					</GridContainer>
+				</div>
+			</div>
+		</Fragment>
+	);
+};
+
+export default About;
