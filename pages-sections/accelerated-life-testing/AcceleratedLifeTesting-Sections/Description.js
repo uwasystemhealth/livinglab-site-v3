fbd25d35a9e446ab673dd5805f6cdadf43b57352
frinzekt <frinzekt@gmail.com>
2020-08-05 15:30:16 +0800

    accelerated life testing content changes

--- pages-sections/accelerated-life-testing/AcceleratedLifeTesting-Sections/Description.js
+++ pages-sections/accelerated-life-testing/AcceleratedLifeTesting-Sections/Description.js
@@ -13,15 +13,15 @@ const ALTDescription = () => {
 			<h2 className={classes.title}>Accelerated Life Testing with the Living Lab</h2>
 			<h5 className={classes.description}>
 				Accelerated Lifecycle Testing (ALT) is used to simulate the environmental and operational conditions expected over the lifecycle of
-				a product in a short period of time.
+				a product in an accelerated time-frame.
 			</h5>
 			<h5 className={classes.description}>
 				This test will identify how the product should perform over its lifecycle and the likely failure modes under conditions it will
 				experience.
 			</h5>
 			<h5 className={classes.description}>
-				The LivingLab have access to a wide range of test equipment on the UWA Campus as well as the ability to design/ repurpose equipment
-				for specific accelerated life trials.
+				The Living Lab has access to a wide range of test equipment on the UWA campuses as well as the ability to design and repurpose
+				equipment for specific accelerated life trials.
 			</h5>
 		</div>
 	);
