b6f25b3b9c5b57393aae7440fe5d140fdf428512
frinzekt <frinzekt@gmail.com>
2020-08-14 18:08:53 +0800

    next-seo

---
+++ next-seo.config.js
@@ -0,0 +1,28 @@
+export default {
+	title: 'Living Lab',
+	description:
+		'The Living Lab @UWA is a team of engineers equipped to help innovators test and prepare products for trial to resources company customers. We accelerate your path to market for IoT-related products by providing Accelerated Life Testing and a real-life trials. We design, build and execute bespoke testing equipment and tests to try and fail your products by generating realistic failure modes in an accelerated time scale. We have procedures and legal agreement templates to enable testing and can help you deploy them.',
+	canonical: 'https://livinglabproject.com/',
+	author: 'Frinze Erin Lapuz',
+	openGraph: {
+		type: 'website',
+		locale: 'en_au',
+		url: 'https://livinglabproject.com/',
+		site_name: 'Living Lab',
+		title: 'Living Lab',
+		images: [
+			{
+				url: 'https://livinglabproject.com/img/android-chrome-512x512.png',
+				width: 512,
+				height: 512,
+				alt: 'Og Image Alt',
+			},
+			{
+				url: 'https://livinglabproject.com/img/android-chrome-192x192.png',
+				width: 192,
+				height: 192,
+				alt: 'Og Image Alt Second',
+			},
+		],
+	},
+};
