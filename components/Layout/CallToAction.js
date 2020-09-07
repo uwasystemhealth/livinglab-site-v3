a802237ea4d436c3b7c63af9006ce436a7492487
frinzekt <frinzekt@gmail.com>
2020-09-07 13:58:24 +0800

    Added brochure download

--- components/Layout/CallToAction.js
+++ components/Layout/CallToAction.js
@@ -3,6 +3,9 @@ import React, { Fragment, useContext } from 'react';
 // nodejs library that concatenates classes
 import classNames from 'classnames';
 
+// MATERIAL UI ICON
+import DownloadIcon from '@material-ui/icons/GetApp';
+
 // material-ui core components
 import { makeStyles } from '@material-ui/core/styles';
 
@@ -11,6 +14,8 @@ import Button from 'components/MaterialKit/CustomButtons/Button.js';
 
 //OWN COMPONENTS
 import Context from 'components/Context/index';
+import GridContainer from 'components/MaterialKit/Grid/GridContainer.js';
+import GridItem from 'components/MaterialKit/Grid/GridItem.js';
 
 // STYLES
 import styles from 'assets/jss/nextjs-material-kit/components/callToActionStyle.js';
@@ -37,9 +42,20 @@ const CallToAction = (props) => {
 						with innovators we’re perfectly placed to support WA-based businesses.
 					</p>
 					<br />
-					<Button color='primary' onClick={openContactFormModal}>
-						Get Involved
-					</Button>
+					<GridContainer justify="center" alignItems="center">
+						<GridItem xs={8}>
+							<GridContainer>
+								<GridItem md={6}>
+									<Button color='primary' onClick={openContactFormModal}>
+										Get Involved
+									</Button>
+								</GridItem>
+								<GridItem md={6}>
+									<Button href="https://drive.google.com/file/d/1qF03s0Mela-LMPy3dd5s6hI0qKuQndnX/view?usp=sharing" color="success" external><DownloadIcon/>	Download Brochure</Button>
+								</GridItem>
+							</GridContainer>
+						</GridItem>
+					</GridContainer>
 				</div>
 			</section>
 		</Fragment>
