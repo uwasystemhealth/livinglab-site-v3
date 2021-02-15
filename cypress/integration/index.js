8117d997bb4f6e35d85c8e45cc464f7a7be1fc71
Frinze Erin Lapuz <frinzekt@gmail.com>
2021-02-16 01:00:14 +0800

    720p snapshots and segmented screenshot matching

--- cypress/integration/index.js
+++ cypress/integration/index.js
@@ -25,21 +25,25 @@ describe('Image Difference', () => {
 
   fullUrlEndpoints.forEach((currentPageUrl) => {
     const endpoint = currentPageUrl.replace(`${PAGE_URL}/`, "").replace("/", "=")
-    // Match Each of the Endpoint in its previous snapshot
-    it(`Renders the page - ${endpoint}`, () => {
-      cy.visit(currentPageUrl)
-    })
 
     it(`Image Difference on 720p Screens - ${endpoint}`, () => {
+      cy.visit(currentPageUrl)
       cy.viewport(1280, 720)
-      cy.matchImageSnapshot();
 
+      const viewportHeight = Cypress.config("viewportHeight")
+      cy.document().then(doc => {
+        const scrollHeight = doc.documentElement.offsetHeight
+        const numberOfScreenshot = Math.ceil(scrollHeight / viewportHeight)
+        for (let index = 0; index < numberOfScreenshot; index++) {
+          // Scroll down a viewport Height
+          cy.scrollTo(0, viewportHeight * index)
+          cy.wait(500) // Wait for a little on scrolling
+          cy.matchImageSnapshot(
+            `${endpoint || "Homepage"}/720p/${index + 1}-${numberOfScreenshot}`,
+            { capture: "viewport" }
+          )
+        }
+      })
     })
-
-    it(`Image Difference on Iphone 6+ Screens - ${endpoint}`, () => {
-      cy.viewport("iphone-6+")
-      cy.matchImageSnapshot();
-    })
-
   })
 })
